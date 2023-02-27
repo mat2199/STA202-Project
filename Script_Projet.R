53fb01fb99ec986c3fbef25e675435c529e52dac
Occhipinti Mathieu <mathieu.occhipinti@ensta-paris.fr>
2023-02-27 11:05:09 +0100

    Création d'un vecteur date avec la librairie xts et concaténation avec notre data frame d'origine

--- Script_Projet.R
+++ Script_Projet.R
@@ -1,25 +1,37 @@
-
-#Import library
 library(xts)
+
+library(tidyr)
 library(imputeTS)
 
-#Import Data
+
 setwd("C:/Users/mathi/OneDrive/Documents/ENSTA PARIS/Cours_deuxième_année/STA202/Projet_STA202")
+
 air_data<-read.table("AirQualityUCI.csv",header=T,sep=";")
+print(air_data)
 
 
-air_data<-air_data[,1:15]
+air_data<-air_data[2:9356,3:15]
+
+
+
+
+
 head(air_data)
 
 
+#Create vector date 
 
-#Create a Date variable 
 date1<- strptime("03/10/2004 18:00:00", "%m/%d/%Y %H:%M:%S")
-date2<- strptime("04/04/2005 13:00:00", "%m/%d/%Y %H:%M:%S")
+date2<- strptime("04/04/2005 14:00:00", "%m/%d/%Y %H:%M:%S")
 Date_air<-seq.POSIXt(date1,date2, by = "1 hour")
 
 
-air_data.xts<-xts(air_data,order.by=Date_air)
+# Merge the two data frames together
+
+
+air_data_final<-cbind(Date,air_data)
+
+#air_data.xts<-xts(air_data,order.by=Date_air)
 
 plot(air_data$CO.GT)
 
@@ -28,3 +40,5 @@ test<-air_data$NMHC.GT.
 test<-replace(test,test<0,NA)
 print(test)
 test1<-na_mean(test)
+
+plot(test1,type='l')
