5d7a3805f73ab12e049967fc61664dcd051e1928
DaGrapix <theluffy62@outlook.fr>
2023-02-27 20:38:53 +0100

    plots

--- Script_Projet.R
+++ Script_Projet.R
@@ -14,16 +14,27 @@ head(air_data)
 
 
 #Create a Date variable 
-date1<- strptime("03/10/2004 18:00:00", "%m/%d/%Y %H:%M:%S")
-date2<- strptime("04/04/2005 13:00:00", "%m/%d/%Y %H:%M:%S")
+date1<- strptime("03/10/2004 18:00:00", "%d/%m/%Y %H:%M:%S")
+date2<- strptime("04/04/2005 14:00:00", "%d/%m/%Y %H:%M:%S")
 Date_air<-seq.POSIXt(date1,date2, by = "1 hour")
 
 
 air_data.xts<-xts(air_data,order.by=Date_air)
 
-plot(air_data$CO.GT)
+#####
+#plots
 
-test<-air_data$NMHC.GT.
+#plot(air_data$CO.GT)
+plot(air_data$PT08.S1.CO)
+#plot(air_data$NMHC.GT)
+plot(air_data$PT08.S2.NMHC)
+plot(air_data$PT08.S3.NOx)
+#plot(air_data$NO2.GT)
+plot(air_data$PT08.S4.NO2)
+
+# PT08.S1.CO   PT08.S2.NMHC   PT08.S3.NOx   PT08.S4.NO2
+
+
 
 test<-replace(test,test<0,NA)
 print(test)
