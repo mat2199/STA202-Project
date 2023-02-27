3715d93fe1969399749398ce268fb8279ac41a41
mat2199 <106100039+mat2199@users.noreply.github.com>
2023-02-27 10:28:10 +0100

    Update Script_Projet.R
    Ajout de quelques commentaires

--- Script_Projet.R
+++ Script_Projet.R
@@ -1,23 +1,22 @@
+
+#Import library
 library(xts)
-
-
 library(imputeTS)
 
-
+#Import Data
 setwd("C:/Users/mathi/OneDrive/Documents/ENSTA PARIS/Cours_deuxième_année/STA202/Projet_STA202")
-
 air_data<-read.table("AirQualityUCI.csv",header=T,sep=";")
+
 
 air_data<-air_data[,1:15]
 head(air_data)
 
 
-air_data_with_NA<-replace(air_data,-200,NA)
 
-
+#Create a Date variable 
 date1<- strptime("03/10/2004 18:00:00", "%m/%d/%Y %H:%M:%S")
 date2<- strptime("04/04/2005 13:00:00", "%m/%d/%Y %H:%M:%S")
-Date_air<-seq(date1,date2, by = "1 hour")
+Date_air<-seq.POSIXt(date1,date2, by = "1 hour")
 
 
 air_data.xts<-xts(air_data,order.by=Date_air)
